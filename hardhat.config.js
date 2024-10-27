32bf955363ea308fa504d3a4e1d1c42245b978c6
zzzk1 <madzhou1@gmail.com>
2024-10-27 09:23:00 +0800

    devlop fundme on sepolia and verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { vars } = require("hardhat/config");
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY_1 = vars.get("SEPOLIA_PRIVATE_KEY_1");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY_1],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  }
 };
