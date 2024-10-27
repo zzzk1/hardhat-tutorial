46809a95d0b5c7b6a606011d946642dba8340afe
zzzk1 <madzhou1@gmail.com>
2024-10-27 16:13:41 +0800

    pass dataFeed address by construction

--- ignition/modules/FundMe.ts
+++ ignition/modules/FundMe.ts
@@ -1,10 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { vars } from "hardhat/config";
+
 const THREE_MINS = 180;
+const USD_ETH_AGGREGATOR_V3_ADDRESS = vars.get("USD_ETH_AGGREGATOR_V3_ADDRESS");
 
 export default buildModule("FundMe", (m) => {
     const lockTime = m.getParameter("lockTime", THREE_MINS);
 
-    const fundMe = m.contract("FundMe", [lockTime])
+    const fundMe = m.contract("FundMe", [lockTime, USD_ETH_AGGREGATOR_V3_ADDRESS])
 
     return { fundMe };
 });
